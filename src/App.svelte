a669d9d6cda692d4fd9b72748f2dd7cb0c9bdfc8
napat35253 <nbhahol@gmail.com>
2021-09-22 23:51:06 +0700

    second update

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
 	import Intro from './views/Intro.svelte';
+	import Info from './views/Info.svelte';
+	import Header from './views/Header.svelte'
 </script>
 
-
+<Header/>
 <Intro/>
+<Info/>
